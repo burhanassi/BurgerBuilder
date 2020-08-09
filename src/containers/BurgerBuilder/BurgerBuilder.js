02b115a213655de0bf390cfe7742ee610f4837ab
burhanassi <61707044+burhanassi@users.noreply.github.com>
2020-08-09 08:37:49 +0300

    9-8-2020 8:37am [before redux]

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -78,25 +78,17 @@ class  BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        // alert("You continue!");
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            costumer: {
-                name: 'Ali',
-                address: {
-                    street: 'Teststreet',
-                    city: 'Ramallah'
-                },
-                email: 'test@test.com',
-            },
-            deliveryMethod: 'Normal'
+        // // alert("You continue!");
+
+        const queryParams = [];
+        for (let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order).then(response => {
-            this.setState({loading: false, purchasing: false});
-        }).catch(error => {
-            this.setState({loading: true, purchasing: false});
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
         });
     }
 
